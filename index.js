6d9e19397b34c45df9b5a88e6114c6543b850f8e
Jieun55 <ohje7530@naver.com>
2022-02-08 06:35:33 +0900

    Javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+function changeText1(){
+    var h2 = document.getElementsByTagName('h2');
+    h2[0].innerHTML = "모각코 화이팅"
+}
+
+function changeText2(){
+    var h2 = document.getElementsByTagName('h2');
+    h2[1].innerHTML = "각잡고 열심히 하면"
+}
+
+function changeText3(){
+    var h2 = document.getElementsByTagName('h2');
+    h2[2].innerHTML = "코린이 탈출이다"
+}
