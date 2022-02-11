5131aeb49c2998a397519d7a2d47acce3283d5a2
Jieun55 <ohje7530@naver.com>
2022-02-11 16:21:32 +0900

    Javascript

---
+++ JS_Day5.js
@@ -0,0 +1,11 @@
+var n = 0;
+var str = "안녕하세요";
+var trueValue = true;
+var k;
+var kitty = {name : "고양이", age:5};
+
+console.log(typeof(n));
+console.log(typeof(str));
+console.log(typeof(trueValue));
+console.log(typeof(k));
+console.log(typeof(kitty));
