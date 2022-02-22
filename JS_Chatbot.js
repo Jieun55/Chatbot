76e84e8d8cbcc1281b3e3706a6764bc85979a239
Jieun55 <ohje7530@naver.com>
2022-02-22 17:19:58 +0900

    Chatbot

--- JS_Chatbot.js
+++ JS_Chatbot.js
@@ -1,14 +1,15 @@
 function check_text() {
 	var value = document.getElementById("console").value;
-	var catsays = document.getElementById("catsays")
+	var catsays = document.getElementById("catsays");
 	
-	if(value == "alert") {
-		alert("경고창이 떴습니다.");
+	if(value == "불 켜줘!") {
+		document.body.style.backgroundColor = '#ffffff';
+		catsays.innerHTML = "불을 켰다옹!";
 	}
 	else if(value == "불 좀 꺼줄래?"){
 		document.body.style.backgroundColor = 'black';
 		catsays.style.color = "gray";
-		catsays.innerHTML = "먀옹!!!"
+		catsays.innerHTML = "먀옹!!!(너무 어두워!)";
 	}
 	else{
 		console.log(value);
